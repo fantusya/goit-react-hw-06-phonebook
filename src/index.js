7ab7341a63e312d4d428e5359448653ad04ad8de
Anastasiia <guba.anastaysha@gmail.com>
2022-11-26 22:49:20 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/App';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './constants/theme';
-import './index.css';
+import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
 import 'react-toastify/dist/ReactToastify.css';
+
+import './index.css';
+import { store } from './redux/store';
+// import { store, persistor } from './redux/store';
+import App from 'components/App';
+import { theme } from './constants/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        {/* <PersistGate persistor={persistor}> */}
+        <App />
+        {/* </PersistGate> */}
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
